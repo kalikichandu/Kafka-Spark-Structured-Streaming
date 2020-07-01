eeb018d93105ef58c61803243ca66b51e5839089
kalikichandu <kaliki.chandrasekhar@tcs.com>
2020-07-01 10:23:00 +0530

    initial changes

---
+++ Consumer/consumer.py
@@ -0,0 +1,16 @@
+from time import sleep
+import os
+from kafka import KafkaConsumer
+from json import loads
+
+consumer = KafkaConsumer(
+    'test',
+     bootstrap_servers=['10.0.1.4:9092'],
+     auto_offset_reset='earliest',
+     enable_auto_commit=True,
+     group_id='my-group',
+     value_deserializer=lambda x: loads(x.decode('utf-8')))
+
+for message in consumer:
+    message = message.value
+    print('{}'.format(message))
