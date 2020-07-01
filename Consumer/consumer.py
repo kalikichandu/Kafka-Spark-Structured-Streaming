bafebe19b7fc5dd0d96ab7e54815e9c3001cd47d
KALIKI CHANDRASEKHAR REDDY <kalikichandrasekhar@gmail.com>
2020-07-01 11:20:12 +0530

    Update consumer.py

--- Consumer/consumer.py
+++ Consumer/consumer.py
@@ -8,7 +8,7 @@ consumer = KafkaConsumer(
      bootstrap_servers=['10.0.1.4:9092'],
      auto_offset_reset='latest',
      enable_auto_commit=True,
-     group_id='my-group',
+     #group_id='my-group',
      value_deserializer=lambda x: loads(x.decode('utf-8')))
 
 for message in consumer:
