245e37a72f08eec46099e9ed23869e0018d6a647
KALIKI CHANDRASEKHAR REDDY <kalikichandrasekhar@gmail.com>
2020-07-01 11:02:47 +0530

    Update consumer.py

--- Consumer/consumer.py
+++ Consumer/consumer.py
@@ -4,9 +4,9 @@ from kafka import KafkaConsumer
 from json import loads
 
 consumer = KafkaConsumer(
-    'test',
+    'multibroker',
      bootstrap_servers=['10.0.1.4:9092'],
-     auto_offset_reset='earliest',
+     auto_offset_reset='latest',
      enable_auto_commit=True,
      group_id='my-group',
      value_deserializer=lambda x: loads(x.decode('utf-8')))
